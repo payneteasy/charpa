04f10383e0ea34c5f402f4978ba06f0277c95e7f
esinev <esinev@3280897e-2c1b-11de-958b-d3ca58845339>
2010-10-18 13:13:29 +0000

    Fixed for compile

--- trunk/charpa/charpa-web/src/main/java/com/googlecode/charpa/web/page/progress/ProgressPage.java
+++ trunk/charpa/charpa-web/src/main/java/com/googlecode/charpa/web/page/progress/ProgressPage.java
@@ -1,5 +1,6 @@
 package com.googlecode.charpa.web.page.progress;
 
+import com.googlecode.charpa.progress.service.IProgressInfoService;
 import com.googlecode.charpa.web.page.BasePage;
 import com.googlecode.charpa.web.progress.ProgressPanel;
 import org.apache.wicket.PageParameters;
@@ -10,6 +11,8 @@ import org.apache.wicket.PageParameters;
 public class ProgressPage extends BasePage {
 
     public ProgressPage(PageParameters aParameters) {
-        add(new ProgressPanel("progress-panel", aParameters));
+        add(new ProgressPanel("progress-panel", aParameters, theProgressInfoService));
     }
+
+    IProgressInfoService theProgressInfoService = null;
 }
