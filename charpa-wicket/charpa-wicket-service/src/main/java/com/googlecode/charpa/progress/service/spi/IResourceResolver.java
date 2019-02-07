d1287c160c6f9330bfe69450d7c65ade18c7a3e6
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-02-07 16:39:11 +0400

    Remove charpa-wicket and set a pointer to the new location

--- charpa-wicket/charpa-wicket-service/src/main/java/com/googlecode/charpa/progress/service/spi/IResourceResolver.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.charpa.progress.service.spi;
-
-import java.io.Serializable;
-
-public interface IResourceResolver extends Serializable {
-	String resolve(String key, String defaultValue);
-}
--- charpa-wicket/charpa-wicket-web/src/main/java/com/googlecode/charpa/web/progress/ProgressParameters.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.charpa.web.progress;
-
-/**
- * Parameters for progress
- */
-public class ProgressParameters {
-    public static final String NEXT_PAGE = "nextPage";
-}
